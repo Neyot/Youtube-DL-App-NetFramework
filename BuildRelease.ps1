eddcaeab2f9eed6508b0129cbea6bd98d3d76bc6
Neyot <almorgan9@gmail.com>
2021-03-28 22:14:35 +0100

    Automatic build script using Powershell and MSBuild. Version build options include true/false variables that will increment the respective version number. Another option is to 'override' the version so we can set it to a specific version instead of incrementing.

---
+++ BuildRelease.ps1
@@ -0,0 +1,64 @@
+Get-Variable -Exclude PWD,*Preference | Remove-Variable -EA 0
+# VERSION BUILD OPTIONS:
+
+$IncreaseMajor = $false
+$IncreaseMinor = $false
+$IncreaseBuild = $false
+$IncreaseRevision = $true
+
+#$OverrideVersion = '0.1.1.1'
+
+# BUILD SCRIPT BELOW:
+
+$localPath=(Get-Location).toString() + "\"
+
+if (Test-Path variable:OverrideVersion) {
+	$NewVersion = $OverrideVersion
+} else {
+	msbuild Build.xml /t:BuildForVersionCheck
+
+	.\Release\NoBinaries\Youtube-DL-App.exe --version | Out-Null
+
+	$VersionFile = Get-Content ($localPath + "version.txt")
+
+	$MajorVersion = $VersionFile[0] -as [int]
+	$MinorVersion = $VersionFile[1] -as [int]
+	$BuildVersion = $VersionFile[2] -as [int]
+	$RevisionVersion = $VersionFile[3] -as [int]
+
+	if ($IncreaseMajor) {
+		$MajorVersion = $MajorVersion + 1;
+	}
+	if ($IncreaseMinor) {
+		$MinorVersion = $MinorVersion + 1;
+	}
+	if ($IncreaseBuild) {
+		$BuildVersion = $BuildVersion + 1;
+	}
+	if ($IncreaseRevision) {
+		$RevisionVersion = $RevisionVersion + 1;
+	}
+
+	$NewVersion = '{0}.{1}.{2}.{3}' -f $MajorVersion, $MinorVersion, $BuildVersion, $RevisionVersion
+}
+
+$NewVersion.Split('.') | Out-File ($localPath + "version.txt")
+#Get-Content -Path ($localPath + "version.txt")
+
+$AssemblyFiles = Get-ChildItem -Path .\ -Filter AssemblyInfo.cs -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName }
+$VersionText = 'Version("{0}")' -f $NewVersion
+
+foreach ($file in $AssemblyFiles) {
+	if (Test-Path $file -PathType leaf) {
+		$content = Get-Content $file
+		$content -replace 'Version\(".*"\)',$VersionText > $file
+	}
+}
+
+if (Test-Path -Path "Binaries\") {
+    msbuild Build.xml /p:Configuration=Release /p:VersionNumber=$NewVersion /t:ReleaseWithBinaries
+} else {
+    msbuild Build.xml /p:Configuration=Release /p:VersionNumber=$NewVersion /t:ReleaseNoBinaries
+}
+
+"`r`nBuilt new release with version {0}.`r`n" -f $NewVersion
